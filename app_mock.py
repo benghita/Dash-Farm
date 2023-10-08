f067930d1df0c61e6e248aeeb3a7bae831fd9d55
ghita benjrinija <benjrinijaghita@gmail.com>
2023-10-08 10:01:58 +0100

    add CV2

--- app_mock.py
+++ app_mock.py
@@ -10,50 +10,14 @@ import plotly.graph_objs as go
 import pandas as pd
 import numpy as np
 from scipy import signal
-from preprocessor import Preprocessor
-from tensorflow.keras.models import load_model
-import joblib
+from predictor import Predictor
 
-# Load and preprocess DF
-preprocessor = Preprocessor()
-#df = preprocessor.df
-#summary = preprocessor.summary(df)
-#date = preprocessor.last_recorded_date
-#df = df [['Date', 'CV1', 'MV1', 'MV2', 'MV3', 'MV4']]
+# Define object Predictor
+predictor = Predictor()
 
-# Load ML tools
-model_path = 'ml_tools/P075_24h_3h_CNNLSTM.h5'
-scaler1_path = 'ml_tools/scaler1.pkl'
-scaler2_path = 'ml_tools/scaler2.pkl'
-       
-model = load_model(model_path)
-
-with open(scaler1_path, 'rb') as file1:
-    sc1 = joblib.load(file1)
-
-with open(scaler2_path, 'rb') as file2:
-    sc2 = joblib.load(file2)
-
-# Define function to predict 3h
-def predict_3h(df, date, features):
-    #date = df[['Date']].max()
-    X_test = df[features][:1440]
-    X_test_sc = sc1.transform(X_test)
-    X_test_sc = X_test_sc[np.newaxis, :, :]
-    y = model.predict(X_test_sc)
-    y_pred_reshaped = y.reshape(-1, y.shape[-2])
-    y_pred = sc2.inverse_transform(y_pred_reshaped)
-
-    date_rng = pd.date_range(start=date, periods=180, freq='1min')
-    # Create a DataFrame with the array and the DatetimeIndex
-    df_pred = pd.DataFrame(y_pred.T, index=date_rng)
-
-    # Rename the columns to 'date' and 'prediction'
-    df_pred = df_pred.reset_index()
-    df_pred.columns = ['Date', 'prediction']
-
-    return df_pred
-
+# Fetch data ('Date', 'CV1', 'MV1') of all availble dfs in mongo API
+dfs = predictor.fetch()
+predictions = predictor.predict_3h(cv = 2)
 
 # Setting up Dash app
 app = dash.Dash(
@@ -74,25 +38,10 @@ theme = {
     "detail": "#1F7A8C",
 }
 
-# Function to display the date and time of the last record
-def last_record_div():
-    _, date = preprocessor.request_24()
-    return html.Div(
-        className="row power-settings-tab",
-        children=[
-            # Title
-            html.Div(
-                className="Title",
-                children=[html.H3(
-                    f"Last Record : {date.year}-{date.strftime('%B')}-{date.day} {date.hour}:{date.minute}", id="power-title", #className='title-line'
-                    )]),
-        ],
-    )
-
 # Create an empty list to hold the components for each column
 column_components = []
 
-columns = ['CV1', 'MV1', 'MV2', 'MV3', 'MV4']
+columns = ['CV1', 'CV2']
 
 # Create a row for each column in the dataset
 for index, column in enumerate(columns):
@@ -117,23 +66,20 @@ for index, column in enumerate(columns):
                 ], className="four columns", style={'text-align': 'center', 'flex': '10',}),
         ], className="row", style = {"width":"100%", 'display': 'flex', 'width': '95%', 'padding':'15px'}))
     
-
 # Call back to update the colors of the graphs in the table after changing the theme
 @app.callback(
-    [Output(f"column-graph-{column}", "figure") for column in columns],
-    [Input("toggleTheme", "value"), Input('interval-component', 'n_intervals')],
+    [Output(f"column-graph-{column}", "figure") for column in columns],#+[Output('DFs_list', 'data')]+[Output('predictions_list', 'data')],
+    [Input("toggleTheme", "value")],
 )
-def update_column_graphs(theme_value, interval):
+def update_column_graphs(theme_value):
     theme_select = "dark" if theme_value else "light"
     axis = axis_color[theme_select]
     marker = marker_color[theme_select]
     
-    df, _ = preprocessor.request_24()
     graph_outputs = []
-
-    for column in columns:
+    for df in dfs:
         base_figure = dict(
-            data=[dict(x=df["Date"], y=df[column], marker={"color": marker})],
+            data=[dict(x=df["Date"], y=df["CV1"], marker={"color": marker})],
             layout=dict(
                 xaxis=dict(
                     #title="Date",
@@ -156,7 +102,7 @@ def update_column_graphs(theme_value, interval):
             ),
         )
         graph_outputs.append(base_figure)
-    return graph_outputs
+    return graph_outputs#, dfs, predictions
 
 # Define list of ids for each variable
 link_ids = [{'type': 'column-link', 'index': column} for column in columns]
@@ -164,11 +110,11 @@ link_ids = [{'type': 'column-link', 'index': column} for column in columns]
 # Call back to update the main graph after select a variable or changing the theme
 @app.callback(
     Output("main-graph", "figure"),
-    [Input("toggleTheme", "value")] + [Input('interval-component', 'n_intervals')]
+    [Input("toggleTheme", "value")]# + [Input('interval-component', 'n_intervals')]
     + [Input({'type': 'column-link', 'index': column}, 'n_clicks') for column in columns],
     #prevent_initial_call=True
 )
-def update_selected_column(theme_value, interval , *n_clicks):
+def update_selected_column(theme_value, *n_clicks):
     selected_index = n_clicks.index(max(n_clicks[1:]))
     selected_column = columns[selected_index]
 
@@ -177,16 +123,15 @@ def update_selected_column(theme_value, interval , *n_clicks):
         selected_column = "CV1"
 
     theme_select = "dark" if theme_value else "light"
-    selected_column = "prediction"
-    column = 'CV1'
     axis = axis_color[theme_select]
     marker = marker_color[theme_select]
-    df, date = preprocessor.request_24()
-    df_3 = predict_3h(df, date, ['MV1', 'CV1'])
+
+    prediction = predictions[selected_index]
+
     # Create a trace for the selected column
     trace = go.Scatter(
-        x=df_3["Date"],
-        y=df_3[selected_column],
+        x=prediction["Date"],
+        y=prediction['prediction'],
         mode='lines',
         marker={"color": marker},
         line={'width': 3},  # Set the line thickness
@@ -195,15 +140,15 @@ def update_selected_column(theme_value, interval , *n_clicks):
 
     # Create horizontal lines for min and max expected values
     min_expected = go.Scatter(
-        x=df_3["Date"],
-        y=[preprocessor.thresholds[column][0]] * len(df),
+        x=prediction["Date"],
+        y=[predictor.thresholds[0]] * len(prediction),
         mode='lines',
         line=dict(color='red', dash='dash'),
         name='Min Expected'
     )
     max_expected = go.Scatter(
-        x=df_3["Date"],
-        y=[preprocessor.thresholds[column][1]] * len(df),
+        x=prediction["Date"],
+        y=[predictor.thresholds[1]] * len(prediction),
         mode='lines',
         line=dict(color='red', dash='dash'),
         name='Max Expected'
@@ -244,7 +189,7 @@ def update_selected_column(theme_value, interval , *n_clicks):
      Output('frozen-gauge', 'label'),
      Output('missing-gauge', 'label'),
      Output('outlier-gauge', 'label')],
-    [Input({'type': 'column-link', 'index': column}, 'n_clicks') for column in columns]+[Input('interval-component', 'n_intervals')],
+    [Input({'type': 'column-link', 'index': column}, 'n_clicks') for column in columns],#+[Input('interval-component', 'n_intervals')],
 )
 def update_gauges_and_column(*n_clicks):
     selected_index = n_clicks.index(max(n_clicks[1:]))
@@ -253,11 +198,11 @@ def update_gauges_and_column(*n_clicks):
     if selected_column is None:
         # Default to "CV1" if no link is clicked
         selected_column = "CV1"
-    df, _ = preprocessor.request_24()
-    summary = preprocessor.summary(df)
-    frozen = int(summary[summary['Variable'] == selected_column]['Frozen'].item())
-    missing = int(summary[summary['Variable'] == selected_column]['Missing'].item())
-    outlier = int(summary[summary['Variable'] == selected_column]['Outlier'].item())
+
+    summary = predictor.CV_sammary(selected_index)
+    frozen = int(summary[0])
+    missing = int(summary[1])
+    outlier = int(summary[2])
     # Format the labels with the displayed values
     frozen_label = f"Frozen {frozen}%"
     missing_label = f"Missing {missing}%"
@@ -270,6 +215,8 @@ app.layout = html.Div(
     id="main-page",
     className='full-screen-div',
     children=[
+        dcc.Store(id='DFs_list'),
+        dcc.Store(id='predictions_list'),
         # Header
         html.Div(
             id="header",
@@ -387,7 +334,7 @@ app.layout = html.Div(
                                                         style = {'display': 'flex', 'width': '100%','font-size': '16px', 'padding': '10px'},
                                                         #className="knobs",
                                                     )], ),
-                                            last_record_div(),
+                                            html.Div(id='last_record_date'),
                                             html.Div(
                                                 className="row power-settings-tab",
                                                 children=[
@@ -408,15 +355,30 @@ app.layout = html.Div(
                 ),
             ],
         ),
-        dcc.Interval(
-        id='interval-component',
-        interval=60 * 1000,  # in milliseconds (1 minute)
-        n_intervals=0
-        ),
-        dcc.Store(id="control-inputs", data={}),
+        #dcc.Interval(id='interval-component',interval=60 * 1000, n_intervals=0),
     ],
 )
 
+# Function to display the date and time of the last record
+#@app.callback(
+#        Output("last_record_date", "children"),
+#        [Input('interval-component', 'n_intervals')]
+#)
+#def last_record_div(interval):
+#    dfs = dcc.Store(id='CVs_list').data
+#    df = dfs[0]
+#    date = date = df[['Date']].max()
+#    return html.Div(
+#        className="row power-settings-tab",
+#        children=[
+#            # Title
+#            html.Div(
+#                className="Title",
+#                children=[html.H3(
+#                    f"Last Record : {date.year}-{date.strftime('%B')}-{date.day} {date.hour}:{date.minute}", id="power-title", #className='title-line'
+#                )]),
+#        ],
+#    )
 # Callback updating backgrounds
 @app.callback(
     [
--- predictor.py
+++ predictor.py
@@ -13,11 +13,12 @@ import requests
 
 
 class Predictor :
-    def __init__(self, data_path = '/Users/ghita/Desktop/fm/projects/AGRO-Dashboard/P075-6 Months_MVCV_18June23.csv',
-                model_path = '/Users/ghita/Desktop/fm/projects/AGRO-Dashboard/ml_tools/P075_24h_3h_CNNLSTM.h5',
-                scaler1_path = '/Users/ghita/Desktop/fm/projects/AGRO-Dashboard/ml_tools/scaler1.pkl',
-                scaler2_path = '/Users/ghita/Desktop/fm/projects/AGRO-Dashboard/ml_tools/scaler2.pkl'):
+    def __init__(self, ml_tools_path = 'ml_tools/', num_cvs = 2):
         
+        # Define the threshold of CV
+        self.thresholds = [20, 450]
+
+        # Define the URL and the header to connect to mongodb API
         self.url = "https://eu-west-2.aws.data.mongodb-api.com/app/data-rpqya/endpoint/data/v1/action/find"
 
         self.headers = {
@@ -26,56 +27,111 @@ class Predictor :
             "Accept": "application/json",
             }
         
-        self.thresholds = [25.10530472, 472.3631897]
-        
-    def fetch(self, cv):
-        if cv == 1 : 
-            collection = "P075"
-        else :
-            collection = "P092"
+        # Define ML tools (models and scalers) for each CV
+        self.models = []
+        self.scalers_1 = []
+        self.scalers_2 = []
+        for i in range(1, num_cvs+1) :
+            self.models.append(load_model(ml_tools_path+'model_'+str(i)+'.h5'))
 
-        # Specify the fields to project in the "projection" field of the payload
-        payload = json.dumps({
-            "dataSource": "Cluster0",
-            "database": "agrodaraa",
-            "collection": collection,
-            "sort": {"Date": -1},
-            "limit": 1440,
-            "projection": {"CV1": 1, "MV1": 1, "Date": 1}
-        })
+            with open(ml_tools_path+'scaler1_'+str(i)+'.pkl', 'rb') as file1:
+                self.scalers_1.append(joblib.load(file1))
 
-        # Make the POST request
-        response = requests.request("POST", self.url, headers = self.headers, data = payload)
+            with open(ml_tools_path+'scaler2_'+str(i)+'.pkl', 'rb') as file2:
+                self.scalers_2.append(joblib.load(file2))
+
         
-        # Check if the request was successful
-        if response.status_code == 200:
-            data = response.json()
-        else:
-            print(f"Request failed with status code {response.status_code}")
         
-        # Extract the list of dictionaries
-        documents_list = data['documents']
+    def fetch(self):
+
+        self.documents_list = []
+        self.dfs = []
+        for collection in ['P075', 'P092'] :
+            # Specify the fields to project in the "projection" field of the payload
+            payload = json.dumps({
+                "dataSource": "Cluster0",
+                "database": "agrodaraa",
+                "collection": collection,
+                "sort": {"Date": -1},
+                "limit": 1440,
+                "projection": {"CV1": 1, "MV1": 1, "Date": 1}
+            })
+            # Make the POST request
+            response = requests.request("POST", self.url, headers = self.headers, data = payload)
+            # Check if the request was successful
+            if response.status_code == 200:
+                data = response.json()
+            else:
+                print(f"Request failed with status code {response.status_code}")
+            
+            # Extract the list of dictionaries
+            df = pd.DataFrame(data['documents'])
+            self.dfs.append(df)
+            # Fill null values with forward fill (ffill)
+            df.fillna(method='ffill', inplace=True)
+            #df['Date'] = pd.to_datetime(df['Date'], format="%Y-%m-%dT%H:%M:%SZ")
+            self.documents_list.append(df)
 
         # Create a DataFrame from the list of dictionaries
-        return pd.DataFrame(documents_list)
+        return self.documents_list
 
     def CV_sammary (self, cv):
-        data = df[[column]].copy()
+        df = self.dfs[cv]
+        data = df[['CV1']].copy()
         total_rows = len(data)
         thr = 5
         # Mark outliers
-        outliers = (data < self.thresholds[column][0]) | (data > self.thresholds[column][1])
+        outliers = (data < self.thresholds[0]) | (data > self.thresholds[1])
         # Calculate the percentage of outliers for this variable
         percentage_outliers = (outliers.sum() / len(data)) * 100
 
         # Count NaN and missing values
-        missing_count = data[column].isnull().sum()
+        missing_count = data['CV1'].isnull().sum()
         percentage_missing_count = (missing_count.sum() / len(data)) * 100
 
         # Calculate the frequency of each unique value
-        unique_values = data[column].value_counts()
+        unique_values = data['CV1'].value_counts()
             # Calculate the percentage of frozen values for this variable
         frozen_count = sum(unique_values[unique_values <= thr])
         percentage_frozen = (frozen_count / total_rows) * 100
 
-        return percentage_outliers[column], percentage_missing_count, percentage_frozen
+        return [percentage_frozen, percentage_missing_count, percentage_outliers['CV1']]
+    
+    def predict_3h(self, cv, features = ['MV1','CV1']):
+        # Define list of predictions
+        self.predictions = []
+        for i in range(cv):
+
+            # Define ML tools for each variabe 
+            sc1 = self.scalers_1[i]
+            sc2 = self.scalers_2[i]
+            model = self.models[i]
+            df = self.documents_list[i]
+
+            # Last Record Date
+            date = df[['Date']].max()
+
+            # Prepare data
+            X_test = df[features][:1440]
+            #X_test=np.array(X_test, dtype=np.float32)
+            X_test_sc = sc1.transform(X_test)
+            X_test_sc = X_test_sc[np.newaxis, :, :]
+
+            # Prediction : 
+            y = model.predict(X_test_sc)
+            y_pred_reshaped = y.reshape(-1, y.shape[-2])
+            y_pred = sc2.inverse_transform(y_pred_reshaped)
+            
+            # Add column fot Date
+            date_rng = pd.date_range(start=date['Date'], periods=180, freq='1min')
+            # Create a DataFrame with the array and the DatetimeIndex
+            df_pred = pd.DataFrame(y_pred.T, index=date_rng)
+
+            # Rename the columns to 'date' and 'prediction'
+            df_pred = df_pred.reset_index()
+            df_pred.columns = ['Date', 'prediction']
+
+            self.predictions.append(df_pred)
+
+        return self.predictions
+
